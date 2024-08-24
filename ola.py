53426ebbf9410b28ed4cd9ec1c0ff4f1b84f033b
GeorgeLarc <jorgeluisarchiboldcortes12@gmail.com>
2024-08-24 17:51:09 -0500

    Create ola.py

---
+++ ola.py
@@ -0,0 +1,14 @@
+meme_dict = {
+            "CRINGE": "Algo excepcionalmente raro o embarazoso",
+            "LOL": "Una respuesta común a algo gracioso",
+            "Meme": "Imagen o video cómico que se comparte y modifica rápidamente en internet",
+            "Troll": "Persona que intencionalmente provoca o molesta a otros en línea"
+            }
+            
+word = input("Escribe una palabra que no entiendas (¡con mayúsculas!): ")
+if word in meme_dict.keys():
+    print(meme_dict.keys)
+    
+   
+else:
+    print("no se encontro la palabra")
